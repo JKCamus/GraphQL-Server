937528e6e08e66833d01a1ba3115013e25e0e07a
wujuming <juming.wu@smartx.com>
2022-09-28 16:12:42 +0800

    feat: test graphql-yoga

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
-import express from "express";
-import { graphqlHTTP } from "express-graphql";
-import cors from "cors";
-import { buildSchema } from 'graphql';
+import { createServer } from '@graphql-yoga/node'
 
-
-const PORT = 9090;
-
-const schema = buildSchema(` 
-  type Query { 
-    hello: String
-  }
-`);
-
-const resolvers = {
-  hello: () => "Hello world!",
-};
-
-const app = express();
-app.use(cors());
-app.use(
-  "/graphql",
-  graphqlHTTP((req) => ({
-    schema,
-    rootValue: {
-      ...resolvers,
+const server = createServer({
+  schema: {
+    typeDefs: /* GraphQL */ `
+      type Query {
+        hello: String
+      }
+    `,
+    
+    resolvers: {
+      Query: {
+        hello: () => 'Hello from Yoga!',
+      },
     },
-    graphiql: true //开启调试工具
-  }))
-);
-app.listen(process.env.PORT ?? PORT);
+  },
+})
 
-console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
+server.start()
